036b0a87d49ebebcf8b4b251b066829932aca9ca
dieDeMiguel <diedemiguel@gmail.com>
2020-06-24 01:17:11 +0200

    added logic to post route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -12,25 +13,19 @@ router.get('/items', function(req, res, next) {
   res.send('Here you will find the array parsed');
 });
 
-// GET item in the array
-router.get('/items/:id', function(req, res, next) {
-  res.send('Fetch an object whithin the array by :id');
-});
+// // GET item in the array
+// router.get('/items/:id', function(req, res, next) {
+//   res.send('Fetch an object whithin the array by :id');
+// });
 
 //Create an Item
-router.post('/item', function(req, res, next) {
-  res.semd('');
+router.post('/items', function(req, res, next) {
+  let item = JSON.stringify({'id': req.body.id, 'name': req.body.name});
+  
+  fs.appendFileSync('items.txt', item, (err) => {
+    if(err) throw err;
+  console.log("Item was saved into memory");
+  });
 });
-
-//Update an item in the array
-router.put('/item/:id', function(req, res, next) {
-  res.send('');
-});
-
-//Delete an item by :id
-router.delete('/item/:id', function(req, res, next) {
-  res.send('');
-})
-
 
 module.exports = router;
