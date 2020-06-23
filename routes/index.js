286fe789c17009615401d25ef379723de80d8e60
dieDeMiguel <diedemiguel@gmail.com>
2020-06-24 00:07:27 +0200

    create all basic routing for CRUD

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,34 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Esta App esta dedicada a Charly Ivor' });
+  res.render('index', { title: 'Express' });
 });
+
+
+// GET objects array complete
+router.get('/items', function(req, res, next) {
+  res.send('Here you will find the array parsed');
+});
+
+// GET item in the array
+router.get('/items/:id', function(req, res, next) {
+  res.send('Fetch an object whithin the array by :id');
+});
+
+//Create an Item
+router.post('/item', function(req, res, next) {
+  res.semd('');
+});
+
+//Update an item in the array
+router.put('/item/:id', function(req, res, next) {
+  res.send('');
+});
+
+//Delete an item by :id
+router.delete('/item/:id', function(req, res, next) {
+  res.send('');
+})
+
 
 module.exports = router;
