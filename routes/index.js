d3c37bde4a3f2deeebae5168084d682621fbcf99
dieDeMiguel <diedemiguel@gmail.com>
2020-06-28 18:53:46 +0200

    error handling '/items' for GET and POST routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
+var appToUse = express()
 var router = express.Router();
 const fs = require('fs');
+const app = require('../app');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -11,60 +13,57 @@ router.get('/', function(req, res, next) {
 // GET objects array complete
 router.get('/items', function(req, res, next) {
     const readFile = 'items.csv'
-    try {
       fs.readFile(readFile, (err, file) => {
         if (err) {
-          throw err;
+          const error = new Error('The file doesn\'t exist');
+          error.status = 417,
+          next(error);
         } 
-        res.status(201).send(data);
+        res.status(201).send(file);
       })
-    } catch(e) {
-     res.status(500).send(e);
-    }
 });
   
 
-
-// // GET item in the array
-// router.get('/items/:id', function(req, res, next) {
-//   res.send('Fetch an object whithin the array by :id');
-// });
-
 //Create an Item
 router.post('/items', function(req, res, next) {
-
   if(req.body.id && req.body.name) {
     newItem = JSON.stringify({'id': req.body.id, 'name': req.body.name});
     fs.writeFile('items.csv', newItem, (err) => {
       if(err) throw err;
-      console.log('The file has been sent');
+      console.log('The file has been created');
       res.status(201).send(newItem);
     })
   } else if(req.body.id) {
-    res.send('The item must have a name')
+    var error = new Error('The item must have a name');
+    error.status = 412;
+    next(error);
   } else if(req.body.name) {
-    res.send('The item must have an Id')
+    var error = new Error('The item must have an Id')
+    error.status = 412;
+    next(error);
   } else {
-    res.send('The item must have name and id')
+    var error = new Error('The item must have an id and a name');
+    error.status = 412;
+    next(error);
   }
-
- 
-
-
-  // if(req.body) {
-  //   let item = JSON.stringify({'id': req.body.id, 'name': req.body.name});
-  //   try {
-  //     fs.appendFile('items.csv', item, (err) => {
-  //       if(err) throw err;
-  //       console.log("Item was saved into memory");
-  //       res.status(201).send(req.body);
-  //     });
-  //   } catch(e) {
-  //    res.status(500).send(e);
-  //   }
-  // }
-
 });
+
+  appToUse.use((req, res, next) => {
+    let error = new Error({
+      status : '404',
+      error : 'Not found'
+    });
+
+  appToUse.use((error, req, res, next) => {
+    res.status(error.status || 500).send({
+      error: {
+        message: error.message || 'Internal server error',
+        status: error.status || 500
+      }
+    })
+  })
+})
+
 
 
 module.exports = router;
