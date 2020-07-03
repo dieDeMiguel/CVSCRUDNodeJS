b47545f71a02a87e56045e4b80ba1f0ef04f62cb
dieDeMiguel <diedemiguel@gmail.com>
2020-07-03 17:55:11 +0200

    GET  and POST routes for Items updated

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const fs = require('fs');
-const {createItem} = require('../controllers/items');
+const {createItem, readFileFromMemory } = require('../controllers/items');
 const storeItem = require('../utils/storeitem');
 
 
@@ -14,19 +14,18 @@ router.get('/', function(req, res, next) {
 
 // GET items 
 router.get('/items', function(req, res) {
-    const fileName = 'items.csv'
-      readFile(fileName, (error, file) => {
-        if (error) {
-          res.send({error}).status(400);
-        } 
-        res.status(200).send(file);
-      })
+    const fileName = req.body.fileName;
+    readFileFromMemory(fileName, (error, response) => {
+      error == null ? res.status(200).send(response) : res.status(400).send(error);
+    })
 });
+
 
 //Create an Item with error handling
 router.post('/items', function(req, res) {
-  createItem(req.body, storeItem)
-  res.send(req.body);
+  createItem(req.body, (error, response) => {
+    error == null ? res.status(201).send(response) : res.status(412) .send(error);
+  });
 });
 
 
