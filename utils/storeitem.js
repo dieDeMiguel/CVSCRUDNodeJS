2ad05090c8ded8872adcb6011ef1063dc2993c5b
dieDeMiguel <diedemiguel@gmail.com>
2020-07-04 21:40:03 +0200

    fileName is not static any longer

--- utils/storeitem.js
+++ utils/storeitem.js
@@ -1,16 +1,15 @@
 const fs = require('fs');
 
-function storeItem(objectToStore, callback) {
-    objectToStore.item.name = objectToStore.item.name.trim();
-    stringItem = JSON.stringify(objectToStore.item);
-    const fileName = 'items.csv';
-    fs.writeFile(fileName, stringItem, (error, response) => {
+function storeItem(objectToStore, callback) {   
+    objectToStore.name = objectToStore.name.trim();
+    stringItem = JSON.stringify(objectToStore);
+    fs.writeFile(objectToStore.fileName, stringItem, (error, response) => {
         if(error) {
             callback('There was an error', null);
         } else {
-            callback(null, 'Object stored correctly. Object name: "' + objectToStore.item.name + '". Object ID: "' + objectToStore.item.id + '"');
+            callback(null, 'Object stored correctly. Object name: "' + objectToStore.name + '". Object ID: "' + objectToStore.id + '"');
         }
-    });
+    })
 }
 
 
