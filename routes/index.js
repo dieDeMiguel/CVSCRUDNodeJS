e6e0ffb3407c3bc395b825a12fc213e47f9f5d8c
dieDeMiguel <diedemiguel@gmail.com>
2020-07-04 21:37:52 +0200

    update item route added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const {createItem, readFileFromMemory, getByID } = require('../controllers/items');
-
-
-
+const {createItem, readFileFromMemory, getByID, updateItem } = require('../controllers/items');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -27,14 +24,18 @@ router.post('/items', function(req, res) {
   });
 });
 
-//Fetch an object by ID
+//Fetch an object by ID, the fileName is on req.body.filename and the ID is part of the URL
 router.get('/items/:id', function(req, res) {
   getByID(req.body.fileName, req.params.id, (error, response) => {
     error == null ? res.status(200).send(response) : res.status(400).send(error);
   })
 })
 
-
-
+//Update an Item, req.body contains the changes in the item, the ID of the item is whithin the URL
+router.patch('/items', function(req, res) {
+  updateItem(req.body, (error, response) => {
+    error == null ? res.status(202).send(response) : res.status(400).send(error);
+  })
+})
 
 module.exports = router;
