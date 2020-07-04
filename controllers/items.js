1debb09cdca4f4ea83168a686e4a0a1a5d3b3728
dieDeMiguel <diedemiguel@gmail.com>
2020-07-04 21:37:31 +0200

    functions added in order to perform the CRUD

--- controllers/items.js
+++ controllers/items.js
@@ -1,41 +1,28 @@
 const fs = require('fs');
-
 const storeItem = require('../utils/storeitem');
 const readFile = require('../utils/readfile');
-const validateData = require('../utils/validation');
+const {validateDataName, validateDataFileName } = require('../utils/validation');
 const fetchOneByID = require('../utils/getoneitem');
 
 const  createItem = (item, callback) =>{
-    if(validateData(item)) {
-       storeItem({item}, (error, response) => {
+    if(validateDataName(item.name, item.id)) {
+       storeItem(item, (error, response) => {
           if(error) {
               callback(error, null);
           } else {
               callback(null, response);
           }
        });
+    } else {
+        callback("The item must have a numeric ID and a name larger than 3 words", null)
     }
 }
 
 const readFileFromMemory = (fileName, callback) => {
-    readFile(fileName, (error, response) => {
-        if(error) {
-            callback(error, null)
-        } else {
-            callback(null, response);
-        }
-    })
-}
-
-const getByID = (fileName, id, callback) => {
-    const item = {
-        id: id,
-        name: fileName
-    }
-    if(validateData(item)) {
-        fetchOneByID(item, (error, response) => {
+    if(validateDataFileName(fileName)) {
+        readFile(fileName, (error, response) => {
             if(error) {
-                callback(error, null);
+                callback(error, null)
             } else {
                 callback(null, response);
             }
@@ -43,8 +30,47 @@ const getByID = (fileName, id, callback) => {
     }
 }
 
+const getByID = (fileName, id, callback) => {
+    if(validateDataFileName(fileName, id)) {
+        fetchOneByID(fileName, id, (error, response) => {
+            if(error) {
+                callback(error, null);
+            } else {
+                callback(null, response);
+            }
+        })
+    } else {
+        callback("The item must have a numeric ID and a file name larger to 5 words", null)
+    }
+}
+
+
+//Update Item route.
+const updateItem = (itemToUpdate, callback) => {
+    if(validateDataName(itemToUpdate.name, itemToUpdate.id)) {
+        fetchOneByID(itemToUpdate.fileName, itemToUpdate.id, (error, response) => {
+            if(error) {
+                callback(error, null);
+            } else {
+                response.name = itemToUpdate.name;
+                response.fileName = itemToUpdate.fileName;
+                storeItem(response, (error, response )=> {
+                    if(error) {
+                        callback(error, null)
+                    } else {
+                        callback(null, response);
+                    }
+                })
+            }
+        })
+    } else {
+        callback("The item must have a numeric ID and a name larger than 3 words", null)
+    }
+}
+ 
 module.exports = {
     createItem: createItem,
     readFileFromMemory: readFileFromMemory,
-    getByID: getByID
+    getByID: getByID,
+    updateItem: updateItem
 };
