2365e9f39053737369f561ac655a586ecb0160a0
dieDeMiguel <diedemiguel@gmail.com>
2020-07-21 15:09:50 +0200

    change mistake in router

--- routes/users.js
+++ routes/users.js
@@ -3,10 +3,11 @@ var router = express.Router();
 const usersController = require('../controllers/users');
 
 /* GET users listing. */
-router.get('/register', function(req, res) {
+router.post('/register', function(req, res) {
   usersController.createUser(req.body.name, req.body.lastName, req.body.email, req.body.password, (error, response) => {
     error == null ? res.status(200).send(response) : res.status(400).send(error);
   })
 });
+
 
 module.exports = router;
