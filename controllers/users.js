648a527ae90daf4be53650819e12e89617515f89
dieDeMiguel <diedemiguel@gmail.com>
2020-07-23 21:03:32 +0200

    creatUser now returns an object . Get allUsers and updateUser mockups are included

--- controllers/users.js
+++ controllers/users.js
@@ -1,22 +1,25 @@
 const fs = require('fs');
 const { checkEmailIsUnique } = require('../utils/checkEmailIsUnique');
 const { storeItem } = require('../utils/storeitem');
-const { makeUserObject } = require('../utils/makeUserObject');
 const { getItemByEmail } = require('../utils/getItemByEmail');
+const { readFile } = require('../utils/readfile');
+const { deleteItem } = require('../utils/deleteItem');
 const fileName = 'users.json';
 
-const createUser = (name, lastName, email, password, callback) => {
-    checkEmailIsUnique(email, fileName, (error) => {
+const createUser = (user, callback) => {
+    checkEmailIsUnique(user.email, fileName, (error) => {
         if(error) {
-            callback(error, null);
-            return;
+            return callback(error, null);
         } else {
-            newUser = makeUserObject(name, lastName, email, password);
-            storeItem(newUser,fileName, (error, response) => {
+            storeItem(user,fileName, (error, response) => {
                 if(error) {
                     callback(error, null);
                 } else {
-                    callback(null, response);
+                    callback(null, {
+                        name: user.name,
+                        lastName: user.lastName,
+                        email: user.email
+                    })
                 }
             });
         }
@@ -33,7 +36,45 @@ const getUser = (email, callback) => {
     })
 }
 
+const getAllUsers = (callback) => {
+    readFile(fileName, (error, response) => {
+        if(error) {
+            callback(error, null);
+        } else {
+            callback(null, response);
+        }
+    });
+}
+
+const updateUser = (data, callback) => {
+    getItemByEmail(fileName, data.email, (error, response) => {
+        if(error) {
+            callback(error, null);
+        } else {
+            deleteItem
+            if(data.name) {
+                response.name = data.name;
+            } 
+            if(data.lastName) {
+                response.lastName = data.lastName;
+            } 
+            if(data.password) {
+                response.password = data.password;
+            } 
+            storeItem(response, fileName, (error, response) => {
+                if(error) {
+                    callback(error, null);
+                } else {
+                    callback(null, response);
+                }
+            });
+        }
+    });
+}
+ 
 module.exports = {
     createUser,
-    getUser
+    getUser,
+    getAllUsers, 
+    updateUser
 }
