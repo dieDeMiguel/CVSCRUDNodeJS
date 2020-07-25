8bb0c5dc444a27b07ac7b7c41ba882687ea1ba10
dieDeMiguel <diedemiguel@gmail.com>
2020-07-25 15:56:51 +0200

    deleted unnecessary routes

--- routes/users.js
+++ routes/users.js
@@ -9,22 +9,8 @@ router.post('/register', function(req, res) {
   })
 });
 
-/* Get one user by email */
-router.get('/:email', function(req, res) {
-  usersController.getUser(req.params.email, (error, response) =>{
-    error == null ? res.status(200).send(response) : res.status(400).send(error);
-  })
-});
-
-/* Get all users */
-router.get('/all', function(req, res) {
-  usersController.getAllUsers((error, response) => {
-    error == null ? res.status(200).send(response) : res.status(400).send(error);
-  })
-});
-
-router.put('/', function(req, res) {
-  usersController.updateUser(req.body, (error, response) => {
+router.post('/login', function(req, res){
+  usersController.login(req.body, (error, response) => {
     error == null ? res.status(200).send(response) : res.status(400).send(error);
   })
 })
