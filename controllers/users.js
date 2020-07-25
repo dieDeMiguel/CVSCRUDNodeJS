c2210546cc9e3bf930744302b4a55a6f6d895927
dieDeMiguel <diedemiguel@gmail.com>
2020-07-25 15:56:26 +0200

    added login route and user must have an email

--- controllers/users.js
+++ controllers/users.js
@@ -1,80 +1,53 @@
 const fs = require('fs');
-const { checkEmailIsUnique } = require('../utils/checkEmailIsUnique');
-const { storeItem } = require('../utils/storeitem');
-const { getItemByEmail } = require('../utils/getItemByEmail');
-const { readFile } = require('../utils/readfile');
-const { deleteItem } = require('../utils/deleteItem');
+const checkEmailIsUnique = require('../utils/checkEmailIsUnique');
+const storeItem = require('../utils/storeitem');
+const getItemByEmail = require('../utils/getItemByEmail');
 const fileName = 'users.json';
 
 const createUser = (user, callback) => {
+    if(!user.email) {
+        return callback('The user must have en email', null)
+    }
     checkEmailIsUnique(user.email, fileName, (error) => {
         if(error) {
             return callback(error, null);
-        } else {
-            storeItem(user,fileName, (error, response) => {
-                if(error) {
-                    callback(error, null);
-                } else {
-                    callback(null, {
-                        name: user.name,
-                        lastName: user.lastName,
-                        email: user.email
-                    })
-                }
-            });
-        }
+        } 
+        storeItem(user,fileName, (error, response) => {
+            if(error) {
+                callback(error, null);
+            } else {
+                callback(null, {
+                    name: user.name,
+                    lastName: user.lastName,
+                    email: user.email
+                })
+            }
+        });
+        
     });
 }
 
-const getUser = (email, callback) => {
-    getItemByEmail(fileName, email, (error, response) => {
+const login = (user, callback) => {
+    getItemByEmail(fileName, user.email, (error, response) => {
         if(error) {
             callback(error, null);
         } else {
-            callback(null, response);
+            if(response.password === user.password) {
+                callback(null, {
+                    message: "User succesfully logged"
+                })
+            } else {
+                callback({
+                    message: 'Wrong password'
+                }, null);
+            }
         }
     })
 }
 
-const getAllUsers = (callback) => {
-    readFile(fileName, (error, response) => {
-        if(error) {
-            callback(error, null);
-        } else {
-            callback(null, response);
-        }
-    });
-}
 
-const updateUser = (data, callback) => {
-    getItemByEmail(fileName, data.email, (error, response) => {
-        if(error) {
-            callback(error, null);
-        } else {
-            deleteItem
-            if(data.name) {
-                response.name = data.name;
-            } 
-            if(data.lastName) {
-                response.lastName = data.lastName;
-            } 
-            if(data.password) {
-                response.password = data.password;
-            } 
-            storeItem(response, fileName, (error, response) => {
-                if(error) {
-                    callback(error, null);
-                } else {
-                    callback(null, response);
-                }
-            });
-        }
-    });
-}
  
 module.exports = {
     createUser,
-    getUser,
-    getAllUsers, 
-    updateUser
+    login
 }
