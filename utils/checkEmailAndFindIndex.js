2a18bfcbaa945f6e54f2ec0d887e53cb77be52cb
dieDeMiguel <diedemiguel@gmail.com>
2020-07-26 23:46:02 +0200

    merge different util methods into one  file, then removed the old methods

--- utils/checkEmailAndFindIndex.js
+++
@@ -1,8 +0,0 @@
-function checkEmailAndFindIndex(item, email, array) {
-    const isMatch = (item) => item.email == email;
-    const index = array.findIndex(isMatch);
-
-    return index;
-}
-
-module.exports = checkEmailAndFindIndex;
