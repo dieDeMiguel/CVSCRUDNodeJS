839b972389cda39c0aaf72532f9253affe2cf8d6
dieDeMiguel <diedemiguel@gmail.com>
2020-07-27 16:06:57 +0200

    new if statement, works better

--- utils/findIndex.js
+++ utils/findIndex.js
@@ -1,10 +1,11 @@
 function findIndex(item, key, array) {
-    if(typeof key == String) {
-        const isMatch = (item) => item.email == key;
+    if(key == parseInt(key)) {
+        const isMatch = (item) => item.id == key;
         const index = array.findIndex(isMatch);
         return index;
+        
     } else {
-        const isMatch = (item) => item.id == key;
+        const isMatch = (item) => item.email == key;
         const index = array.findIndex(isMatch);
         return index;
     }
