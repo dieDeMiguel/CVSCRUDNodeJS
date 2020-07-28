d5aa6c67d6e86ced95360ee76be9ecb74a7c0395
dieDeMiguel <diedemiguel@gmail.com>
2020-07-28 22:18:06 +0200

    now checks only for ID as primary key

--- utils/find.js
+++ utils/find.js
@@ -5,23 +5,15 @@ function find(id, fileName, callback) {
         if(error) {
             callback(error, null); 
         } else {
-            if(id == parseInt(id, 10)) {
-                item = response.find((item) => item.id == id);
-                if(item === undefined) {
-                    callback('Unable to find an item with the provided ID', null);
-                } else {
-                    callback(null, item);
-                }
+            id == parseInt(id, 10);
+            item = response.find((item) => item.userID == id);
+            if(item === undefined) {
+                callback('Unable to find an object with the provided ID', null);
             } else {
-                user = response.find((user) => user.email == id);
-                if(user === undefined) {
-                    callback('Unable to find a user with the provided email', null);
-                } else {
-                    callback(null, user);
-                }
-            }
-        }
-    });   
+                callback(null, item);
+            } 
+        }  
+    });
 }
 
 module.exports = find;
