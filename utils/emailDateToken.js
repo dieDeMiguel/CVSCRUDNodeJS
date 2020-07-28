c398697277226982f3e268b15db1219d80942ea7
dieDeMiguel <diedemiguel@gmail.com>
2020-07-28 22:17:42 +0200

    changed name into generateToken

--- utils/emailDateToken.js
+++
@@ -1,9 +0,0 @@
-function emailDateToken(email) {
-    const date = Date.now().toString();
-    const dateEmail = date + "." + email;
-    let buff = new Buffer(dateEmail);
-    let base64data = buff.toString('base64');
-    return base64data;
-}
-
-module.exports = emailDateToken;
